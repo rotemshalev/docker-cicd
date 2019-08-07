6e4e69077f9065302d4a5c75dbaabdf18708aee4
rotemshalev <rotemsh97@gmail.com>
2019-08-07 13:56:36 +0300

    Add pipelie job dsl

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -43,3 +43,26 @@ job('NodeJS Docker example') {
         }
     }
 }
+
+pipelineJob('DSL_Pipeline') {
+
+  def repo = 'https://github.com/rotemshalev/docker-cicd.git'
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master', '**/feature*')
+          scriptPath('./basics/misc/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+  }
+}
